b21b1a66c699b542c254502c777ab6ea17f72f61
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-08-07 11:22:07 +0300

    fix: cd path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/PauliusU/PP4-supervised-learning.git
-cd PP3-pandas/
+cd PP4-supervised-learning/
 pip install --upgrade pipenv --user
 pipenv install
 pipenv run jupyter notebook PP4_supervised_learning.ipynb
